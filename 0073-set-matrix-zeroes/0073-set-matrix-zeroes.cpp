dadf72ae2ceab42c8547c4d3cfeba3fb61d38608
Punit <kumar.punit0705@gmail.com>
2025-11-27 16:22:45 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return;
-        int n = matrix.size();
-        int m = matrix[0].size();
+    void setZeroes(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
 
-        vector<bool> row(n, false);
-        vector<bool> col(m, false);
-
-        // 1) mark rows and cols
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+        for(int i = 0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
                 }
-
-        // 2) apply zeros using marks (no more reading original zeros)
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (row[i] || col[j])
-                    matrix[i][j] = 0;
+            }
+        }
+        for(int i =0; i<m; i++)
+        {
+            for(int j =0; j<n; j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                {
+                    mat[i][j]=0;
+                }
+            }
+        }
     }
 };
