06cd7d1487ce46074b0f784365f6d35c56a0be4b
Punit <kumar.punit0705@gmail.com>
2025-12-02 23:49:56 +0530

    Time: 2460 ms (16.91%), Space: 34.4 MB (87.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int count =0;
         int sum =0; 
-        for(int i =0; i<n; i++)
+        for(int i =0; i<=n; i++)
         {
             int sum =0;
             for(int j =i; j<n; j++)
