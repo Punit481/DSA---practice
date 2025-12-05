f61b8d68d88b5700f6f062f76b7a22c2428eb18c
Punit <kumar.punit0705@gmail.com>
2025-12-05 23:51:35 +0530

    Time: 3 ms (52.49%), Space: 23.5 MB (95.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,27 +2,42 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& arr) {
         int n = arr.size();
+        int cnt1=0;
+        int cnt2=0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
 
-        vector<int> res;
-        unordered_set<int> check;
-        for(int i=0; i<n; i++)
+        for(int i =0; i<n; i++)
         {
-            if(check.count(arr[i])) continue;
-            check.insert(arr[i]);
-            int count = 0;
-            for(int j=0; j<n; j++)
+            if(cnt1==0 && el2!=arr[i])
             {
-                if(arr[i]==arr[j])
-                {
-                    count++;
-                }
+                cnt1=1;
+                el1=arr[i];
             }
-            
-            if(count>n/3)
+            else if(cnt2==0 && el1!=arr[i])
             {
-             res.push_back(arr[i]);
+                cnt2=1;
+                el2=arr[i];
+            }
+            else if(arr[i]==el1) cnt1++;
+            else if(arr[i]==el2) cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
             }
         }
+        vector<int> res;
+        cnt1=0, cnt2=0;
+        for(int i =0; i<n; i++)
+        {
+            if(arr[i]==el1) cnt1++;
+            if(arr[i]==el2) cnt2++;
+        }
+        int mini = (int)(n/3)+1;
+        if(cnt1>= mini) res.push_back(el1);
+        if(cnt2>= mini) res.push_back(el2);
+        sort(res.begin(), res.end());
         return res;
     }
 };
