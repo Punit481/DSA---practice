42a99e2130a947e4f4d2646c43b445c82cb14e01
Punit <kumar.punit0705@gmail.com>
2025-12-15 18:10:26 +0530

    Time: 40 ms (43.64%), Space: 75.6 MB (41.06%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -20,19 +20,17 @@ public:
             if(list1->val < list2->val)
             {
                 temp->next = list1;
-                temp = list1;
                 list1= list1->next;
             }
             else
             {
                 temp->next = list2;
-                temp = list2;
                 list2= list2->next;   
             }
-
-            if(list1) temp->next= list1;
-            else temp->next = list2;
+            temp = temp->next;
         }
+        if(list1) temp->next= list1;
+        else temp->next = list2;
 
         return dummy->next;
     }
