d83c9cb6057595e0a4b632b714fcf49afe68de21
Punit <kumar.punit0705@gmail.com>
2025-12-15 18:05:01 +0530

    Time: 44 ms (39.37%), Space: 75.7 MB (32.36%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,25 +10,55 @@
  */
 class Solution {
 public:
+    ListNode* mergeList(ListNode* list1, ListNode* list2)
+    {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(list1!=NULL && list2!=NULL)
+        {
+            if(list1->val < list2->val)
+            {
+                temp->next = list1;
+                temp = list1;
+                list1= list1->next;
+            }
+            else
+            {
+                temp->next = list2;
+                temp = list2;
+                list2= list2->next;   
+            }
+
+            if(list1) temp->next= list1;
+            else temp->next = list2;
+        }
+
+        return dummy->next;
+    }
+    ListNode* findMiddle(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!= NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
     ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-        vector<int> arr;
+       if(head==NULL || head->next == NULL) return head;
 
-        while(temp)
-        {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
+       ListNode* middle = findMiddle(head);
+       ListNode* left = head;
+       ListNode* right = middle->next;
+       middle->next = nullptr;
 
-        sort(arr.begin(), arr.end());
-        temp = head;
-        int i =0;
-        while(temp)
-        {
-            temp-> val = arr[i];
-            temp = temp->next;
-            i++;
-        }
-        return head;
+       left = sortList(left);
+       right = sortList(right);
+
+       return mergeList(left,right); 
     }
 };
