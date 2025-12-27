effcfd59e4356e9dc51b34281bb10fe263750d2f
Punit <kumar.punit0705@gmail.com>
2025-12-27 22:53:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.98%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -42,3 +42,31 @@ public:
         return head;
     }
 };
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+int init = [] {
+    ofstream out("user.out");
+    cout.rdbuf(out.rdbuf());
+    for (string str, k_str; getline(cin, str) && getline(cin, k_str); cout << '\n') {
+        const int n = ranges::count(str, ',') + 1;
+        int k = stoi(k_str) % n;
+        if (n == 1 || k == 0) {
+            cout << str;
+            continue;
+        }
+        auto it = str.end() - 1;
+        for (int commas = 0; it != str.begin() && commas != k; --it) if (*it == ',') ++commas;
+        int dist = distance(str.begin(), it) + 2;
+        string rotate = str.substr(dist, str.size() - 1 - dist);
+        str.erase(it + 1, str.end() - 1);
+        str.erase(0, 1);
+        cout << '[' << rotate << ',' << str;
+    }
+    exit(0);
+    return 0;
+}();
