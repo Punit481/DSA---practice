85535a9b955d6e916d9093ae569eafcb1245137c
Punit <kumar.punit0705@gmail.com>
2026-01-24 16:42:20 +0530

    Time: 0 ms (100%), Space: 13.5 MB (97.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,24 +4,22 @@ public:
        int n = nums.size();
        int low = 0;
        int high = n-1;
+       int ans = nums.size();
 
        while (low <= high)
        {
             int mid = (low + high)/2;
 
-            if(target == nums[mid])
+            if(nums[mid] >= target)
             {
-                return mid;
-            }
-            else if(target > nums[mid])
-            {
-                low = mid + 1;
+                ans = mid;
+                high = mid -1;
             }
             else
             {
-                high = mid - 1;
+                low = mid+1;
             }
        } 
-       return low;
+       return ans;
     }
 };
